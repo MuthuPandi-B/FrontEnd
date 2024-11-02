ddf42b1f592469cf76dfbe166ca66b123c5e0350
MuthuPandi-B <muthup61@gmail.com>
2024-11-02 18:04:33 +0530

    updated

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
           Login
         </button>
         <p className="text-center text-sm text-gray-500 mt-4">
-          Don’t have an account?{" "}
+          Don’t have an account?{"/ "}
           <a href="/register" className="text-indigo-500 hover:underline">Sign up</a>
         </p>
         <p className="text-center text-sm text-gray-500 mt-2">
